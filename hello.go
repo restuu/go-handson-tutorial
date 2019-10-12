13a691ad9ea2c1443c25fbb7c02f50d4f32301cd
restuu <restu_u@outlook.com>
2019-10-12 16:37:30 +0700

    Hello with flag

--- hello.go
+++ hello.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	var str string
+	flag.StringVar(&str, "name", "World", "Input your name")
+
+	flag.Parse()
+
+	fmt.Printf("Hello, %s\n", str)
+
 }
